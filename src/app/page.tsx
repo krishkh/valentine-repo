05abd8ea4394e66726b17f904eac7ebb1b331823
krishkh <krishkhanna00@gmail.com>
2025-02-14 18:12:53 +0530

    done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -512,6 +512,7 @@ const DateProposalApp = () => {
                   initial={{ scale: 0.8 }}
                   animate={{ scale: 1 }}
                 >
+                  <div className="text-7xl">WAIT PATIENTLY!!!</div>
                   <button
                     className="absolute top-2 right-2 text-gray-500 hover:text-gray-700"
                     onClick={() => setShowGif(false)}
